f46666bfe559619426c50cfa8eb9bd785b2fad54
Harsh Patel <harshptl714@gmail.com>
2020-08-14 23:34:29 +0530

    Agenda tested

--- lib/retrivingData/eventModel.dart
+++ lib/retrivingData/eventModel.dart
@@ -1,23 +1,76 @@
-class EventModel {
-  var title;
-  var description;
-  var imageLink;
-  var date;
-  var time;
-  var venue;
-  var regLink;
+import 'package:flutter/material.dart';
 
-  EventModel(this.title, this.description, this.imageLink, this.date, this.time,
-      this.venue, this.regLink);
+// class EventModel {
+//   var title;
+//   var description;
+//   var imageLink;
+//   var date;
+//   var time;
+//   var venue;
+//   var regLink;
+//   List<Speaker> speaker;
 
-  EventModel.fromJson(Map<String, dynamic> data) {
-      title = data['title'];
-      description = data['description'];
-      imageLink = data['imageLinks'];
-      date = data['date'];
-      time = data['time'];
-      venue = data['venue'];
-      regLink = data['registrationLink'];
+//   EventModel(this.title, this.description, this.imageLink, this.date, this.time,
+//       this.venue, this.regLink, this.speaker);
+
+//   EventModel.fromJson(Map<String, dynamic> data) {
+//     title = data['title'];
+//     description = data['description'];
+//     imageLink = data['imageLinks'];
+//     date = data['date'];
+//     time = data['time'];
+//     venue = data['venue'];
+//     regLink = data['registrationLink'];
+//     //speaker = data['speakers'];
+//     var list = data['speakers'] as List;
+//     speaker = list.map((i) => Speaker.fromJson(i)).toList();
+//   }
+// }
+
+// class Speaker {
+//   var name;
+//   var post;
+//   var image;
+//   var session;
+
+//   Speaker(this.name, this.post, this.image, this.session);
+
+//   Speaker.fromJson(Map<String, dynamic> data) {
+//     name = data['name'];
+//     post = data['post'];
+//     image = data['image'];
+//     session = data['session'];
+//   }
+// }
+
+class Agenda {
+  // add others
+  List<Data> agendaList;
+
+  Agenda(this.agendaList);
+  Agenda.fromJson(Map<String, dynamic> data) {
+    var list = data['agenda'] as List;
+    agendaList = list.map((i) => Data.fromJson(i)).toList();
   }
+}
 
+class Data {
+  var name;
+  var time;
+  var photo;
+  var duration;
+  var speaker;
+  var description;
+
+  Data(this.name, this.time, this.photo, this.duration, this.speaker,
+      this.description);
+
+  Data.fromJson(Map<String, dynamic> data) {
+    name = data['name'];
+    time = data['time'];
+    photo = data['photo'];
+    duration = data['duration'];
+    speaker = data['speaker'];
+    description = data['description'];
+  }
 }
--- lib/retrivingData/yaml.dart
+++ lib/retrivingData/yaml.dart
@@ -25,31 +25,31 @@
 //     super.initState();
 //   }
 
-  // List<EventModel> _list = List<EventModel>();
-  // Future<List<EventModel>> loadfromAPI() async {
-  //   var res = await http.get(
-  //       'https://raw.githack.com/harshptl14/Club-Gamma-App/Add-ons/events/event_details.yml');
-  //   // print(res.body);
-  //   // print('GotData');
-  //   // var temp = loadYaml(res.body);
-  //   // print(temp);
-  //   var list = List<EventModel>();
-  //   // // var temptojson = json.encode(loadYaml(res.body));
-  //   // // print(temptojson);
-  //   // var jsontolist = json.decode(json.encode(loadYaml(res.body)));
-  //   // return jsontolist;
+// List<EventModel> _list = List<EventModel>();
+// Future<List<EventModel>> loadfromAPI() async {
+//   var res = await http.get(
+//       'https://raw.githack.com/harshptl14/Club-Gamma-App/Add-ons/events/event_details.yml');
+//   // print(res.body);
+//   // print('GotData');
+//   // var temp = loadYaml(res.body);
+//   // print(temp);
+//   var list = List<EventModel>();
+//   // // var temptojson = json.encode(loadYaml(res.body));
+//   // // print(temptojson);
+//   // var jsontolist = json.decode(json.encode(loadYaml(res.body)));
+//   // return jsontolist;
 
-  //   if (res.statusCode == 200) {
-  //     var eventJson = json.decode(json.encode(loadYaml(res.body)));
-  //     for (var eventJson in eventJson) {
-  //       list.add(EventModel.fromJson(eventJson));
-  //     }
-  //   }
-  //   return list;
-  //   // else {
-  //   //   throw Exception('Failed to load Event');
-  //   // }
-  // }
+//   if (res.statusCode == 200) {
+//     var eventJson = json.decode(json.encode(loadYaml(res.body)));
+//     for (var eventJson in eventJson) {
+//       list.add(EventModel.fromJson(eventJson));
+//     }
+//   }
+//   return list;
+//   // else {
+//   //   throw Exception('Failed to load Event');
+//   // }
+// }
 
 //   loadfromAPI() async {
 //     var res = await http.get(
@@ -114,6 +114,8 @@
 // }
 
 import 'dart:convert';
+import 'package:clubgammatesting/photos_activity/data.dart';
+import 'package:clubgammatesting/photos_activity/data.dart';
 import 'package:clubgammatesting/retrivingData/eventModel.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
@@ -134,15 +136,28 @@ class _YamlDemoState extends State<YamlDemo> {
     super.initState();
   }
 
+  // Future loadPage() async {
+  //   var jsonPage = await http.get('https://raw.githubusercontent.com/harshptl14/Club-Gamma-App/Add-ons/events/speaker_details.yml');
+  //   final jsonResponse = json.decode(json.encode(loadYaml(jsonPage.body)));
+  //   Data page = new Data.fromJson(jsonResponse);
+  //   print(page.imagesList[0].name);
+  // }
+
   loadfromAPI() async {
     var res = await http.get(
-        'https://raw.githubusercontent.com/harshptl14/Club-Gamma-App/Add-ons/events/event_details.yml');
-    // print(res.body);
+      'https://raw.githubusercontent.com/harshptl14/Club-Gamma-App/Add-ons/events/agenda_details.yml'
+        //'https://raw.githubusercontent.com/harshptl14/Club-Gamma-App/Add-ons/events/event_details.yml'
+        // 'https://raw.githubusercontent.com/harshptl14/Club-Gamma-App/Add-ons/events/speaker_details.yml'
+        );
+    print('res.body');
+    print(res.body);
     // var temp = loadYaml(res.body);
     // print(temp);
     // var temptojson = json.encode(loadYaml(res.body));
     // print(temptojson);
     var jsontolist = json.decode(json.encode(loadYaml(res.body)));
+    print('JsontoList');
+    print(jsontolist);
     return jsontolist;
   }
 
@@ -172,17 +187,25 @@ class _YamlDemoState extends State<YamlDemo> {
                 itemCount: snapshot.data.length,
                 itemBuilder: (context, index) {
                   var temp =
-                      EventModel.fromJson(snapshot.data.elementAt(index));
+                      Agenda.fromJson(snapshot.data.elementAt(index));
                   return Column(
                     children: [
-                      Text(temp.title),
-                      Text(temp.description),
-                      Image.network(temp.imageLink),
-                      Text(temp.date),
-                      Text(temp.time),
-                      Text(temp.venue),
-                      Text(temp.regLink),
-                      
+                      // Text(temp.speaker[index].post),
+                      // Text(temp.speaker[index].name),
+                      //Image.network(temp.imagesList[index].image),
+                      //Text(temp.time),
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 50.0,
+                        child: ListView.builder(
+                            itemCount: temp.agendaList.length,
+                            itemBuilder: (_, index) {
+                              print(temp.agendaList.length);
+                              print(temp.agendaList[0].time);
+                              print(temp.agendaList[1].time);
+                              return Text(temp.agendaList[index].name);
+                            }),
+                      )
                     ],
                   );
                 });
